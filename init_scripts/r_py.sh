0d461689b7ecc0fc91d54acde4f3b15eaa1af0e4
Florent Bédécarrats <f.bedecarrats@gmail.com>
2024-08-29 00:58:43 +0200

    Update r_py.sh

--- init_scripts/r_py.sh
+++ init_scripts/r_py.sh
@@ -1,4 +1,4 @@
 # Enable setting python environment from RStudio
 export RETICULATE_PYTHON="/usr/bin/python3"
 R -e "install.packages(c('reticulate'))"
-R -e "Sys.setenv(RETICULATE_PYTHON = '/usr/local/bin/python3')"
+R -e "Sys.setenv(RETICULATE_PYTHON = '/usr/bin/python3')"
