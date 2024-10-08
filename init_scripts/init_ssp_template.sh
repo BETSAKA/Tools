c196be9a80d2591b52f34e3456738ca87ef46a52
Florent Bédécarrats <f.bedecarrats@gmail.com>
2024-10-08 21:26:18 +0200

    Add copy from S3

--- init_scripts/init_ssp_template.sh
+++ init_scripts/init_ssp_template.sh
@@ -11,6 +11,9 @@ REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/BETSAKA/${PROJ_NAME}.gi
 git clone $REPO_URL $WORK_DIR
 chown -R onyxia:users $WORK_DIR
 
+# Copy files from s3
+mc cp -r s3/fbedecarrats/diffusion/${PROJ_NAME} $WORK_DIR/data
+
 # Install additional packages passed as arguments
 if [ $# -gt 0 ]; then
     for pkg in "$@"
