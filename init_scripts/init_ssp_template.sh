e5879c5417c7f97b0c2bdd22ae87a75b94433c73
Florent Bédécarrats <f.bedecarrats@gmail.com>
2024-10-20 08:19:42 +0300

    Revert to original startup

--- init_scripts/init_ssp_template.sh
+++ init_scripts/init_ssp_template.sh
@@ -1,25 +1,5 @@
 #!/bin/bash
 
-# Remove existing GDAL and related packages
-apt-get autoremove -y gdal-bin libgdal-dev libgeos-dev libproj-dev 
-
-# Remove 'sf' package in R
-Rscript -e "remove.packages('sf')"
-Rscript -e "remove.packages('terra')"
-
-# Update and install necessary packages
-apt-get update && \
-apt-get install -y software-properties-common && \
-add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable && \
-apt-get update && \
-apt-get install -y libudunits2-dev libgdal-dev libgeos-dev libproj-dev libsqlite0-dev
-
-# Install 'sf' package in R from source
-Rscript -e "install.packages('sf', type = 'source', repos = 'https://cran.r-project.org/')"
-Rscript -e "install.packages('terra', type = 'source', repos = 'https://cran.r-project.org/')"
-
-# Then set-up git and fetch S3 data 
-
 # Variables to be filled-in
 PROJ_NAME=$1
 
