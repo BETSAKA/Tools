7f0b1b9d8e3db35f925c9dd2bda36dc50eb81b11
Florent Bédécarrats <f.bedecarrats@gmail.com>
2024-11-11 18:04:40 +0100

    Create init_ssp_template.sh

---
+++ init_scripts/init_ssp_template.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# Variables to be filled-in
+PROJ_NAME=$1
+
+# Creation of automatic variables
+WORK_DIR=/home/onyxia/work/${PROJ_NAME}
+REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/BETSAKA/${PROJ_NAME}.git # As initial
+
+# Git
+# git config --global user.email "${GIT_USER_MAIL}"
+# git config --global user.name "${GIT_USER_NAME}"
+git clone $REPO_URL $WORK_DIR
+chown -R onyxia:users $WORK_DIR
+
+# Copy files from s3
+mc cp -r s3/fbedecarrats/diffusion/${PROJ_NAME} /home/onyxia/work/
+chown -R onyxia:users $WORK_DIR # make sure users have rights to edit
+
+# Install additional packages passed as arguments 
+if [ $# -gt 0 ]; then
+    for pkg in "$@"
+    do
+        Rscript -e "install.packages('$pkg')"
+    done
+fi
+
+# Set the UI 
+# launch RStudio in the right project
+# Copied from InseeLab UtilitR
+    echo \
+    "
+    setHook('rstudio.sessionInit', function(newSession) {
+        if (newSession && !identical(getwd(), \"'$WORK_DIR'\"))
+        {
+            message('On charge directement le bon projet :-) ')
+            rstudioapi::openProject('$WORK_DIR')
+            # For a slick dark theme
+            rstudioapi::applyTheme('Merbivore')
+            # Console where it should be
+            rstudioapi::executeCommand('layoutConsoleOnRight')
+        }
+    }, action = 'append')
+    " >> /home/onyxia/work/.Rprofile
+
+# Update RStudio keyboard shortcuts configuration
+mkdir -p /home/onyxia/.config/rstudio/keybindings
+cat <<EOT > /home/onyxia/.config/rstudio/keybindings/rstudio_bindings.json
+{
+    "pasteLastYank": ""
+}
+EOT
