2be2a3aca0c7023fc442abf9d8c87ea99e8dcaaf
Florent Bédécarrats <f.bedecarrats@gmail.com>
2024-11-11 17:43:04 +0100

    Update and rename init_ssp_template.sh to init_ssp_no_s3.sh

--- init_scripts/init_ssp_no_s3.sh
+++ init_scripts/init_ssp_no_s3.sh
@@ -13,10 +13,6 @@ REPO_URL=https://${GIT_PERSONAL_ACCESS_TOKEN}@github.com/BETSAKA/${PROJ_NAME}.gi
 git clone $REPO_URL $WORK_DIR
 chown -R onyxia:users $WORK_DIR
 
-# Copy files from s3
-mc cp -r s3/fbedecarrats/diffusion/${PROJ_NAME} /home/onyxia/work/
-chown -R onyxia:users $WORK_DIR # make sure users have rights to edit
-
 # Install additional packages passed as arguments 
 if [ $# -gt 0 ]; then
     for pkg in "$@"
