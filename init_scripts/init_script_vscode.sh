82febd5aa27fb47d5aac606bcd66c4e1f4131209
Edouard Pignède <edouard.pignede@gmail.com>
2025-10-08 15:24:54 +0100

    Update Copilot version and modify chat installation
    Updated Copilot version and commented out chat installation.

--- init_scripts/init_script_vscode.sh
+++ init_scripts/init_script_vscode.sh
@@ -103,16 +103,16 @@ code-server --install-extension pomdtr.excalidraw-editor
 # COPILOT ----------------------------
 
 # Install Copilot (Microsoft's AI-assisted code writing tool)
-copilotVersion="1.234.0"
-copilotChatVersion="0.20.0" # This version is not compatible with VSCode server 1.92.2
+copilotVersion="1.129.0"
+#copilotChatVersion="0.20.0" # This version is not compatible with VSCode server 1.92.2
 
 wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot/${copilotVersion}/vspackage -O copilot.vsix.gz
-wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/${copilotChatVersion}/vspackage -O copilot-chat.vsix.gz
+# wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/${copilotChatVersion}/vspackage -O copilot-chat.vsix.gz
 
 gzip -d copilot.vsix.gz 
-gzip -d copilot-chat.vsix.gz 
+# gzip -d copilot-chat.vsix.gz 
 
 code-server --install-extension copilot.vsix
-code-server --install-extension copilot-chat.vsix
-rm copilot.vsix copilot-chat.vsix
+# code-server --install-extension copilot-chat.vsix
+rm copilot.vsix #copilot-chat.vsix
 
