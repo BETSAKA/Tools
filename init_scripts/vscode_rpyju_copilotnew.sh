e7b462bb95726acd27a26f4e29aa31da7be70c79
Florent Bédécarrats <f.bedecarrats@gmail.com>
2026-05-13 14:24:32 +0200

    Update copy command to include directory structure

--- init_scripts/vscode_rpyju_copilotnew.sh
+++ init_scripts/vscode_rpyju_copilotnew.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mc cp -r s3/projet-betsaka/PA-impact-deforestation/mapme/PA-impact-on-deforestation /home/onyxia/work
+mc cp -r s3/projet-betsaka/PA-impact-deforestation/mapme /home/onyxia/work/PA-impact-on-deforestation
